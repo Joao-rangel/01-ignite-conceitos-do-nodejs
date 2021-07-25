f5620498d380e1d82013cb6a5bf1b684fff5a171
Joao Rangel <joao.vran@gmail.com>
2021-07-24 22:12:36 -0300

    add app CRUD routes

--- src/index.js
+++ src/index.js
@@ -1,41 +1,103 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const userIndex = users.findIndex((user) => user?.username === username);
+
+  response.body = {userIndex, ...users[userIndex]};
+  if (userIndex >= 0) next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+
+  const userExists = users.find((user) => user?.username === username);
+  if (userExists) {
+    return response.status(400).json({error: 'usuário já cadastrado'});
+  };
+
+  const user = {
+    name,
+    username,
+    id: uuidv4(),
+    todos: [],
+  };
+  users.push(user);
+
+  return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {todos} = response.body;
+
+  return response.status(201).json(todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {title, deadline} = request.body;
+  const {userIndex} = response.body;
+
+  const todo = {
+    title,
+    deadline,
+    id: uuidv4(),
+    done: false,
+    created_at: new Date(),
+  };
+
+  users[userIndex].todos.push(todo)
+
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {id} = request.params;
+  const {userIndex} = response.body;
+  const todoPartial = request.body;
+
+  const todoIndex = users[userIndex].todos.findIndex((todo) => todo?.id === id)
+  if (todoIndex < 0){
+   return response.status(404).json({error: 'Todo não localizado'});
+  }
+  Object.assign(users[userIndex].todos[todoIndex], {...todoPartial});
+
+  return response.status(201).json(users[userIndex].todos[todoIndex]);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {id} = request.params;
+  const {userIndex} = response.body;
+
+  const todoIndex = users[userIndex].todos.findIndex((todo) => todo?.id === id)
+  if (todoIndex < 0){
+    return response.status(404).json({error: 'Todo não localizado'});
+  }
+  users[userIndex].todos[todoIndex].done = true;
+
+  return response.status(201).json(users[userIndex].todos[todoIndex]);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {id} = request.params;
+  const {userIndex} = response.body;
+
+  const todoIndex = users[userIndex].todos.findIndex((todo) => todo?.id === id)
+  if (todoIndex < 0){
+    return response.status(404).json({error: 'Todo não localizado'});
+  }
+  users[userIndex].todos.splice(todoIndex, 1);
+
+  return response.status(204).json(users[userIndex].todos[todoIndex]);
 });
 
 module.exports = app;
